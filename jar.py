266b2131d4b82160636081753876d6d2fa7a0e02
Justin Lee <justindavidlee88@gmail.com>
2020-05-14 01:53:46 -0700

    Add Accept class
    * Add languages
    * Add encodings
    * Add MIME types
    * Add charsets

--- jar.py
+++ jar.py
@@ -2,14 +2,46 @@ from collections import OrderedDict
 from wsgiref.simple_server import make_server
 
 
+def parse_accept(value):
+    mapping = {}
+    if value:
+        items = value.split(',')
+        for item in items:
+            if ';' in item:
+                key, q = item.split(';')
+                mapping[key] = float(q.split('=')[1])
+            else:
+                mapping[item] = 1.0
+    return mapping
+
+
+class Accept:
+
+    def __init__(self, environ):
+        self.language = parse_accept(environ.pop('HTTP_ACCEPT_LANGUAGE', None))
+        self.encoding = parse_accept(environ.pop('HTTP_ACCEPT_ENCODING', None))
+        self.mime_type = parse_accept(environ.pop('HTTP_ACCEPT', None))
+        self.charset = parse_accept(environ.pop('HTTP_ACCEPT_CHARSET', None))
+
+
 class Request:
 
     def __init__(self, environ):
         prefix = 'HTTP_'
         self.headers = {}
-        for key, value in environ.items():
-            if key.startswith(prefix):
-                self.headers[key[len(prefix)].lower()] = value
+        keys = [k for k in environ.keys() if k.startswith(prefix)]
+        for key in keys:
+            value = environ.pop(key)
+            self.headers[key[len(prefix)].lower()] = value
+        self.accept = Accept(self.headers)
+
+    @property
+    def host(self):
+        return self.headers['HTTP_HOST']
+
+    @property
+    def user_agent(self):
+        return self.headers['HTTP_USER_AGENT']
 
 
 class Headers(OrderedDict):
@@ -59,5 +91,5 @@ class Application:
 app = Application()
 if __name__ == '__main__':
     httpd = make_server('localhost', 8000, Application())
-    httpd.handle_request()
+    httpd.serve_forever()
 
