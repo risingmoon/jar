41e5ab1aa4109efc4e534a3207fb9750a1f43cf7
Justin Lee <justindavidlee88@gmail.com>
2020-05-14 00:19:59 -0700

    Add Headers class
    * Add body property
    * Add content_type property

--- jar.py
+++ jar.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from wsgiref.simple_server import make_server
 
 
@@ -11,26 +12,47 @@ class Request:
                 self.headers[key[len(prefix)].lower()] = value
 
 
+class Headers(OrderedDict):
+
+    def __iter__(self):
+        for key in super().__iter__():
+            yield key, self[key]
+
+
 class Response:
 
     def __init__(self, body=b'', status_code=200, content_type='text/plain'):
-        self.body = body
         self.status_code = status_code
+
+        self.headers = Headers()
+        self.body = body
         self.content_type = content_type
+
+    @property
+    def body(self, ):
+        return self.content
+
+    @body.setter
+    def body(self, value):
+        self.content = value
+        self.headers['Content-Length'] = str(len(self.content))
+
+    @property
+    def content_type(self):
+        return self.headers['Content-Type']
+
+    @content_type.setter
+    def content_type(self, value):
+        self.headers['Content-Type'] = str(value)
 
 
 class Application:
 
     def __call__(self, environ, start_response):
         request = Request(environ)
-
         response = Response(b'Test')
         status = '200 OK'
-        response_headers = [
-            ('Content-Type', response.content_type),
-            ('Content-Length', str(len(response.body)))
-        ]
-        start_response(status, response_headers)
+        start_response(status, list(response.headers))
         return [response.body]
 
 
@@ -38,3 +60,4 @@ app = Application()
 if __name__ == '__main__':
     httpd = make_server('localhost', 8000, Application())
     httpd.handle_request()
+
